58eb8f3cf85b389f8bcbeecfac225372d8f9f458
jebashajida <jeba.18feb@gmail.com>
2025-01-12 13:04:34 +0600

    Add files via upload

---
+++ final_lab_exam/model/model.php
@@ -0,0 +1,32 @@
+<?php
+
+function getConnection(){
+    $conn = mysqli_connect('127.0.0.1', 'root', '', 'shop_management');
+    return $conn;
+}
+
+function isUsernameTaken($username){
+    $conn = getConnection();
+    $sql = "SELECT id FROM employee WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function registerEmployee($name, $contact, $username, $password){
+    $conn = getConnection();
+    $sql = "insert into employee VALUES('', '{$name}', '{$contact}', '{$username}', '{$password}')";
+    if(mysqli_query($conn, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+?>
---
+++ final_lab_exam/view/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Shop Management System</title>
+</head>
+
+<body>
+    <h3>Employee Login</h3>
+    <form action="login_val.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit">Login</button><br><br>
+        
+    </form>
+    <a href="registration.php">Register here.</a>
+</body>
+</html>
